67788b73a2124f63e19c7370caf4ad44bc322e7f
Matheus Souto <matheushsouto@gmail.com>
2025-08-26 20:16:15 -0300

    feat: add resource views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -11,5 +12,9 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('/alunos', AlunoController::class);
+});
 
 require __DIR__.'/auth.php';
